9d663c6916d46c729d3700ab7cd2fbd358d6fd6f
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-13 00:08:19 -0500

    feat: implement TestManager instantiating and `run` method

--- src/core/TestManager.php
+++ src/core/TestManager.php
@@ -2,14 +2,48 @@
 
 namespace Sherpa\Test\core;
 
+use ReflectionClass;
+use ReflectionException;
 use Sherpa\Sherpa\test\exceptions\InvalidTestClassException;
 
 class TestManager
 {
+    public private(set) string $class;
+
+    public function __construct(string $class)
+    {
+        $this->class = $class;
+    }
+
     /**
-     * Launch Sherpa Test startup script file.
+     * @return array All test methods (having #Test tag)
+     * @throws ReflectionException
+     */
+    private function getTestMethods(): array
+    {
+        $reflection = new ReflectionClass($this->class);
+        $methods = $reflection->getMethods();
+
+        $testMethods = [];
+
+        foreach ($methods as $method)
+        {
+            $attributes = $method->getAttributes(SherpaTest::class);
+
+            if (!empty($attributes))
+            {
+                $testMethods[] = $method->getName();
+            }
+        }
+
+        return $testMethods;
+    }
+
+
+    /**
+     * Launch a Sherpa test.
      * @throws InvalidTestClassException If provided class does
-     *                          not implement Test interface
+     *                                   not implement Test interface
      */
     public static function run(string $testClass): void
     {
@@ -17,5 +51,21 @@ class TestManager
         {
             throw new InvalidTestClassException($testClass);
         }
+
+        $manager = new TestManager($testClass);
+        $tests = $manager->getTestMethods();
+
+        $instance = new $testClass();
+
+        $instance->startup();
+
+        foreach ($tests as $test)
+        {
+            $instance->beforeEachTest();
+            $instance->$test();
+            $instance->afterEachTest();
+        }
+
+        $instance->end();
     }
 }
