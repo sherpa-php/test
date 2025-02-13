793762834ba087ee719d5bc5c22f01088a3c540d
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-12 20:26:09 -0500

    feat: create Sherpa Test ui's overlay function

---
+++ src/ui/ui.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Create the native Sherpa Test overlay.
+ */
+function overlay(): void
+{
+    echo "<div id='sherpa_test__overlay' 
+               style='position: fixed; inset: 0; border: 4px solid green;'></div>";
+}
