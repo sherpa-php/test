9872e28dcda04ff8d35e231883f11b292a36781e
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-13 02:49:27 -0500

    styles: add negative z-index and 50px padding for body
    on Sherpa Test overlay

--- src/ui/ui.php
+++ src/ui/ui.php
@@ -5,9 +5,12 @@
  */
 function overlay(): void
 {
-    echo "<div id='sherpa_test__overlay' 
+    echo "<style>body { padding: 50px; }</style>
+
+          <div id='sherpa_test__overlay' 
                style='
                  position: fixed;
+                 z-index: -1;
                  inset: 0; 
                  border: 6px solid #2fdc2f;
                '></div>";
