4a954fa8345e9f666653a3f1671ebe130214dd33
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-19 17:04:17 -0500

    feat: add `SherpaTest::$name` attribute

--- src/core/SherpaTest.php
+++ src/core/SherpaTest.php
@@ -7,5 +7,10 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class SherpaTest
 {
+    public private(set) ?string $name;
 
+    public function __construct(?string $name = null)
+    {
+        $this->name = $name;
+    }
 }
